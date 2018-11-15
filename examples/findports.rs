224361c66cfbcd3f43e5599889954f75ca6ed4b9
Markus Reiter <me@reitermark.us>
2018-11-15 06:12:17 +0100

    Fix example.

--- examples/findports.rs
+++ examples/findports.rs
@@ -1,32 +1,32 @@
 extern crate dockworker;
+extern crate failure;
 
-use dockworker::error::*;
+use failure::*;
 use dockworker::{Docker, models::ContainerFilters};
-use std::io::{self, Write};
 
-fn find_all_exported_ports() -> Result<()> {
-    let docker = Docker::from_env()?;
-    let filter = ContainerFilters::new();
-    let containers = docker.list_containers(None, None, None, filter)?;
-    for container in &containers {
-        let info = docker.container_info(&container)?;
+fn find_all_exported_ports() -> Result<(), Error> {
+  let docker = Docker::from_env()?;
+  let filter = ContainerFilters::new();
+  let containers = docker.list_containers(None, None, None, filter)?;
+  for container in &containers {
+    let info = docker.container_info(&container)?;
 
-        // Uncomment this to dump everything we know about a container.
-        //println!("{:#?}", &info);
+    // Uncomment this to dump everything we know about a container.
+    // println!("{:#?}", &info);
 
-        if let Some(ports) = info.network_settings.ports {
-            let ports: Vec<String> = ports.keys().cloned().collect();
-            println!("{}: {}", &info.name, ports.join(", "));
-        }
+    if let Some(ports) = info.network_settings.ports {
+      let ports: Vec<String> = ports.keys().cloned().collect();
+      println!("{}: {}", &info.name, ports.join(", "));
     }
-    Ok(())
+  }
+  Ok(())
 }
 
 fn main() {
-    if let Err(err) = find_all_exported_ports() {
-        write!(io::stderr(), "Error: ").unwrap();
-        for e in err.iter() {
-            write!(io::stderr(), "{}\n", e).unwrap();
-        }
+  if let Err(err) = find_all_exported_ports() {
+    eprint!("Error: ");
+    for e in err.iter_chain() {
+      eprintln!("{}\n", e);
     }
+  }
 }
