749be29a4e3d4960baeea51b27416e5bb367ef4b
Markus Reiter <me@reitermark.us>
2018-11-19 12:29:22 +0100

    Convert `Mount` to enum.

--- src/models/mount.rs
+++ src/models/mount.rs
@@ -1,49 +1,70 @@
 use serde::{Serialize, Deserialize, ser::Serializer, de::{self, Deserializer}};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct Mount {
-  pub target: String,
-  pub source: String,
-  #[serde(rename = "Type")]
-  pub mount_type: MountType,
-  pub read_only: Option<bool>,
-  pub consistency: Option<String>,
-  // pub bind_options: Option<BindOptions>,
-  // pub volume_options: Option<VolumeOptions>,
-  // pub tmpfs_options: Option<TmpfsOptions>,
+#[serde(tag = "Type", rename_all = "lowercase")]
+pub enum Mount {
+  #[serde(rename_all = "PascalCase")]
+  Bind   {
+    target: String,
+    source: String,
+    read_only: Option<bool>,
+    consistency: Option<Consistency>,
+    // #[serde(rename = "BindOptions")]
+    // options: Option<BindOptions>
+  },
+  #[serde(rename_all = "PascalCase")]
+  Volume {
+    target: String,
+    source: String,
+    read_only: Option<bool>,
+    consistency: Option<Consistency>,
+    // #[serde(rename = "VolumeOptions")]
+    // options: Option<VolumeOptions>
+  },
+  #[serde(rename_all = "PascalCase")]
+  Tmpfs  {
+    target: String,
+    source: String,
+    read_only: Option<bool>,
+    consistency: Option<Consistency>,
+    // #[serde(rename = "TmpfsOptions")]
+    // options: Option<TmpfsOptions>
+  },
 }
 
 #[derive(Debug, Clone)]
-pub enum MountType {
-  Bind,
-  Volume,
-  Tmpfs,
+pub enum Consistency {
+  Default,
+  Consistent,
+  Cached,
+  Delegated,
 }
 
-impl Serialize for MountType {
+impl Serialize for Consistency {
   fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
     where S: Serializer
   {
     serializer.serialize_str(match *self {
-      MountType::Bind => "bind",
-      MountType::Volume => "volume",
-      MountType::Tmpfs => "tmpfs",
+      Consistency::Default => "default",
+      Consistency::Consistent => "consistent",
+      Consistency::Cached => "cached",
+      Consistency::Delegated => "delegated",
     })
   }
 }
 
-impl<'de> Deserialize<'de> for MountType {
+impl<'de> Deserialize<'de> for Consistency {
   fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
     where D: Deserializer<'de>
   {
     String::deserialize(deserializer)
       .and_then(|s| {
         match s.as_str() {
-          "bind" => Ok(MountType::Bind),
-          "volume" => Ok(MountType::Volume),
-          "tmfps" => Ok(MountType::Tmpfs),
-          unknown => Err(de::Error::unknown_variant(&unknown, &["bind", "volume", "tmfps"])),
+          "default" => Ok(Consistency::Default),
+          "consistent" => Ok(Consistency::Consistent),
+          "cached" => Ok(Consistency::Cached),
+          "delegated" => Ok(Consistency::Delegated),
+          unknown => Err(de::Error::unknown_variant(&unknown, &["default", "consistent", "cached", "delegated"])),
         }
       })
   }
